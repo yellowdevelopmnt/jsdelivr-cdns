dceed5c8dd39d5eadb5f330232358ffb223978d2
vinhutl <72056963+yellowdevelopment@users.noreply.github.com>
2026-04-23 20:22:10 -0500

    obbywithfriendsonbline

---
+++ obbywithfriendsonline/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2448916.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 280358263, 280357467, 280357850, 280357644, 280357550, 280357605, 280357476, 280357606, 280357646, 280357180, 280357161, 280357710, 280357313, 280357613, 280357774, 280357159, 280357510, 280357310, 280358019, 280357329, 280357183, 280357231, 280358017, 280357608, 280358193, 280358200, 280357158, 280357722, 280358214, 280357342, 280357802, 280358259, 280357344, 280357659, 280357199, 280357371, 280357623, 280357526, 280357527, 280357488, 280357664, 280358053, 280357396, 280357539, 280357675, 280357635, 280357829, 280358241, 280357541, 280357208, 280357281, 280357366, 280357597, 280357547, 280358262, 280357548, 280517931 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/280357767/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/280358191/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/280357770/1/ammo.js', 'preload' : true},
+];
